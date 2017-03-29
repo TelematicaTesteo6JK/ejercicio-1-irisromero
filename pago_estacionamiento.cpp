efa52143ece4cef2354325ff703c60f8c16fb04f
irisromero <traceuse.beet@gmail.com>
2017-03-28 23:54:09 -0600

    ejercicio1-irisromero

--- pago_estacionamiento.cpp
+++ pago_estacionamiento.cpp
@@ -24,6 +24,15 @@ void casos_de_prueba(){
 
     cout << "TEST CASES" << endl;
     // Agrega tus casos de prueba
+cout << test_data		espected_result	actual_result  << endl;
+cout << 0			0		 		pago_estacionamiento(0) << endl;
+cout << 3			30				pago_estacionamiento(3)  << endl;
+cout << 5			50				pago_estacionamiento(5)  << endl;
+cout << 6			54				pago_estacionamiento(6)  << endl;
+cout << 8			72				pago_estacionamiento(8)  << endl;
+cout << 10			90				pago_estacionamiento(10)  << endl;
+cout << 11			77				pago_estacionamiento(11)  << endl;
+cout << 15			105				pago_estacionamiento(15)  << endl;
 
 
 
